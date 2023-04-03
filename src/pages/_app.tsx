ddda188f212b58ae7f5180ff5e8987ae3b1bc673
khs <hyeongs2323@gmail.com>
2023-04-03 16:53:28 +0900

    ts-react-master init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#1110",
+  backgroundColor: "whitesmoke",
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }

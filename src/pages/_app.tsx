19f285cee5f352c33316cf2540600032d7a52b3e
khs <hyeongs2323@gmail.com>
2023-04-03 17:19:35 +0900

    feat: styled type & state type setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "styled-components";
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#1110",
-  backgroundColor: "whitesmoke",
-};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,10 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
+import Circle from "../components/Circle";
 
 export default function Home() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <>
+      <Circle bgColor="teal" borderColor="yellow" />
+      <Circle text="im here" bgColor="tomato" />
+    </>
   );
 }
